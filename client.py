345f3d1f657b8f6c3d8b0846d82be829fc56b4a1
Study <ibuly4@gmail.com>
2021-03-24 19:55:47 +0300

    some thoughts about get

--- client.py
+++ client.py
@@ -4,11 +4,30 @@ class Client:
 
     """client for putting and getting metrics """
 
-     def __init__(self, adr, port, timeout=5):
+     def __init__(self, adr, port, timeout=None):
          self.adr = adr
          self.port = port
          self.timeout = timeout
 
-         with socket.socket() as connection:
-             connection.connect((self.adr, seld.port), timeout)
-             
+         self.connection = socket.socket()
+         self.connection.connect((self.adr, seld.port), timeout)
+
+         # with socket.socket() as connection:
+         #     connection.connect((self.adr, seld.port), timeout)
+
+    # def get(self, key):
+    #     data_returned = {}
+    #     self.connection.send(key)
+    #     try:
+    #         data_returned = self.connection.recv()
+    #     except ClientError as e:
+    #         raise
+
+
+
+
+    # def put(self, key, value):
+    #
+
+    def __del__(self):
+        self.connection.close()

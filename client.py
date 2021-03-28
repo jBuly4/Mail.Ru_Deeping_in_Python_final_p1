bdb96b351876622d564566d5011c18aee45b95ed
jBuly4 <ibuly4@gmail.com>
2021-03-28 20:30:40 +0300

    added solution

--- client.py
+++ client.py
@@ -1,18 +1,15 @@
 import socket
 import time
 
-class ClientError(Exception):
 
-    # def __init__(self, text="ClientError"):
-    #     self.text = text
+class ClientError(Exception):
     pass
 
+
 class Client:
-
-
     """client for putting and getting metrics """
-
     def __init__(self, adr, port, timeout=None):
+
         self.adr = adr
         self.port = port
         self.timeout = timeout
@@ -21,11 +18,8 @@ class Client:
         except socket.error as clErr:
             raise ClientError("failed to connect", clErr)
 
-
-         # with socket.socket() as connection:
-         #     connection.connect((self.adr, seld.port), timeout)
-
     def get(self, key):
+
         normal_answ = 'ok'
         key_tosnd = 'get' + ' ' + str(key) + '\n'
         data_returned = {}
@@ -43,13 +37,10 @@ class Client:
                 raise ClientError("failed to recieve data from get request", clErr)
 
         data_recvd = data_recvd[:-2]
-        # print('recieved answer:\n')
-        # print(data_recvd)
         tmp_lst = data_recvd.split('\n') #list of lines
-        # print(tmp_lst)
+        
         if tmp_lst[0] == normal_answ:
             for data in tmp_lst[1:]:
-                # print(data.split(' '))
                 if len(data.split(' ')) != 3:
                     raise ClientError("ClientError")
                 else:
@@ -60,22 +51,15 @@ class Client:
                     data_returned[key_rcvd].append((int(time_rcvd), float(value_rcvd),))
                 except ValueError as clErr:
                     raise ClientError("value and time must be numbers", clErr)
-            # print(data_returned)
-            # data_returned.sort(key=lambda values: values[1][0])
-            # https://stackoverflow.com/questions/37526577/sort-list-of-tuples-in-a-dictionary
+            # https://stackoverflow.com/questions/3121979/how-to-sort-a-list-tuple-of-lists-tuples-by-the-element-at-a-given-index
             for values in data_returned.values():
                 values.sort(key=lambda tuple: tuple[0])
         else:
             raise ClientError("ClientError")
         return data_returned
-        # except ClientError as clErr:
-        #     #print(clErr)
-        #     raise ClientError('ClientError', clErr)
-
-#in data_returned values should be int and float!
-
 
     def put(self, key, value, timestamp=None):
+
         normal_answ = 'ok\n\n'
         if timestamp == None:
             data_to_snd = 'put' + ' ' + str(key) + ' ' + str(value) + ' '  + str(int(time.time())) + '\n'
@@ -90,17 +74,101 @@ class Client:
         except socket.error as clErr:
             raise ClientError("failed to send get request", clErr)
 
-        # print('recieved answer:\n')
-        # print(data_recvd)
         if data_recvd == normal_answ:
             pass
         else:
             raise ClientError("ClientError")
-        # except ClientError as clErr:
-        #     print(clErr)
 
     def __del__(self):
+
         try:
             self.connection.close()
         except socket.error as clErr:
             raise ClientError("failed to close connection", clErr)
+
+''' solution
+
+import bisect
+import socket
+import time
+
+
+class ClientError(Exception):
+    """класс исключений клиента"""
+    pass
+
+
+class Client:
+    def __init__(self, host, port, timeout=None):
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+
+        try:
+            self.connection = socket.create_connection((host, port), timeout)
+        except socket.error as err:
+            raise ClientError("Cannot create connection", err)
+
+    def _read(self):
+
+        data = b""
+
+        while not data.endswith(b"\n\n"):
+            try:
+                data += self.connection.recv(1024)
+            except socket.error as err:
+                raise ClientError("Error reading data from socket", err)
+
+        return data.decode('utf-8')
+
+    def _send(self, data):
+
+        try:
+            self.connection.sendall(data)
+        except socket.error as err:
+            raise ClientError("Error sending data to server", err)
+
+    def put(self, key, value, timestamp=None):
+
+        timestamp = timestamp or int(time.time())
+        self._send(f"put {key} {value} {timestamp}\n".encode())
+        raw_data = self._read()
+
+        if raw_data == 'ok\n\n':
+            return
+        raise ClientError('Server returns an error')
+
+    def get(self, key):
+
+        self._send(f"get {key}\n".encode())
+        raw_data = self._read()
+        data = {}
+        status, payload = raw_data.split("\n", 1)
+        payload = payload.strip()
+
+        if status != 'ok':
+            raise ClientError('Server returns an error')
+
+        if payload == '':
+            return data
+
+        try:
+
+            for row in payload.splitlines():
+                key, value, timestamp = row.split()
+                if key not in data:
+                    data[key] = []
+                bisect.insort(data[key], ((int(timestamp), float(value))))
+
+        except Exception as err:
+            raise ClientError('Server returns invalid data', err)
+
+        return data
+
+    def close(self):
+
+        try:
+            self.connection.close()
+        except socket.error as err:
+            raise ClientError("Error. Do not close the connection", err)
+            '''
